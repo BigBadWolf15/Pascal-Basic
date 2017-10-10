26138c522959719f911885a948604cbf9e972db1
BigBadWolf15 <BigBadWolf15@users.noreply.github.com>
2017-10-10 19:11:20 +0700

    Create new file
    A small example

---
+++ saywhat.pas
@@ -0,0 +1,12 @@
+program saywhat;
+
+uses crt;
+var
+  ch: char;
+
+begin
+  write('Type in any letter ');readln(ch);
+  writeln;
+  writeln('Wow, amazing ! Do you know that ', ch, ' has a value of ', ord(ch), ' in the ASCII Alphabet');
+  readln;
+end.
