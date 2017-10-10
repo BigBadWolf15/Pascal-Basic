17bf8291ff4080d40bee62128d3f04136d6d4cd6
BigBadWolf15 <BigBadWolf15@users.noreply.github.com>
2017-10-10 19:18:52 +0700

    update 1
    nothing to see here, just look at the code

--- saywhat.pas
+++ saywhat.pas
@@ -1,5 +1,3 @@
-program saywhat;
-
 uses crt;
 var
   ch: char;
@@ -10,3 +8,4 @@ begin
   writeln('Wow, amazing ! Do you know that ', ch, ' has a value of ', ord(ch), ' in the ASCII Alphabet');
   readln;
 end.
+(*Programmed by Louiscc, github.com/BigBadWolf15
