406d6ab0d8c033b34c65de184a955b26a18910b6
BigBadWolf15 <BigBadWolf15@users.noreply.github.com>
2020-11-03 08:45:59 -0600

    Add files via upload
    pascal if7, 8 03/11/2020

---
+++ 07.pas
@@ -0,0 +1,35 @@
+﻿{Created by Le Thanh Trung - 03/11/2020}
+
+program If7;
+
+var
+  CSM, CSC, songuoi, sotien, tieuthu: integer;
+
+begin
+  write('Nhap chi so nuoc cu: '); readln(CSC);
+  write('Nhap chi so nuoc moi: '); readln(CSM);
+  write('Nhap so nguoi dang ky: '); readln(songuoi);
+  
+  if (CSM > CSC) then 
+  begin
+    tieuthu := CSM - CSC;
+    writeln('So nuoc da tieu thu: ', tieuthu);
+    if (tieuthu < 4 * songuoi) then begin
+      sotien := 4400 * tieuthu;
+    end else sotien := 4400 * 4 * songuoi;
+    
+    tieuthu := tieuthu - 4 * songuoi;
+    
+    if (tieuthu > 0) and (tieuthu < 2 * songuoi) then begin
+      sotien := sotien + 8300 * tieuthu;
+    end else sotien := sotien + 8300 * 2 * songuoi;
+    
+    tieuthu := tieuthu - 2 * songuoi;
+    
+    if tieuthu > 0 then sotien := sotien + 10500 * tieuthu;
+  
+  end else writeln('Loi nhap du lieu, chuong trinh ket thuc!');
+  
+  writeln('So tien nuoc la: ', sotien);
+  readln;
+end.
---
+++ 08.pas
@@ -0,0 +1,25 @@
+﻿{Created by Le Thanh Trung - 03/11/2020}
+program If8;
+
+var
+  thang, nam, songay: integer;
+
+begin
+  write('Nhap thang: '); readln(thang);
+  write('Nhap nam: '); readln(nam);
+  
+  case thang of
+    1, 3, 5, 7, 8, 10, 12: songay := 31;
+    4, 6, 9, 11: songay := 30;
+  
+    2:
+      begin
+        if (nam mod 400 = 0) or ((nam mod 4 = 0) and (nam mod 100 <> 0)) then
+          songay := 29 else songay := 28;
+      end;
+      
+  else writeln('So thang khong hop le!');
+  end;
+  writeln('Thang ', thang, ' nam ', nam, ' co so ngay la: ', songay, ' ngay.');
+  readln;
+end.
